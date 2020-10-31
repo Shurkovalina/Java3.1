82c615270e14e9ca4cb3eeb24f4ab24f02125fef
Shurkovalina <shurkowalina@yandex.ru>
2020-10-31 15:02:30 +0300

    added Main.java

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketprice = 1281;
+        int numberofmiles = ticketprice / 20;
+        System.out.println(numberofmiles);
+    }
+}
